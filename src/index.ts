687e5907f58c5fa396a868fcec42aca401ba78c0
Asad Mansuri <asad.mansuri@matrixhive.com>
2023-10-18 16:01:46 +0530

    Inital Commit

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+document.getElementById("lorem").onsubmit = async (event) => {
+  event.preventDefault()
+  // Get the currently selected element in the Designer
+  const el = await webflow.getSelectedElement()
+  if (el && el.textContent) {
+    // If we found the element and it has the ability to update the text content,
+    // replace it with some placeholder text
+    el.setTextContent(
+      "Hello World! This is my first Webflow Application!!"
+    )
+    // Finally, save the changes to the element & they will be reflected in the Designer
+    await el.save()
+  }
+}
